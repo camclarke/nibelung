4c5f34fa74bf07f46926676a62d8e4ba672dc04b
Israel Rocha <camclarke@live.com>
2019-11-04 18:53:55 -0400

    home stats

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-
-</div>
+    <div class="container-fluid">
+        <div>
+            <h1>User</h1>
+        </div>
+        <div class="d-flex">
+            <div class="pr-5"><strong>125</strong> First value</div>
+            <div class="pr-5"><strong>456</strong> Second value</div>
+            <div class="pr-5"><strong>789</strong> Third value</div>
+        </div>
+    </div>
 @endsection
