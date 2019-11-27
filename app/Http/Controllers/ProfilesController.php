4c609d5e0a8b1df957f99dbf34bedd92c95f1f69
Israel Rocha <camclarke@live.com>
2019-11-26 22:31:21 -0400

    ProfilesController findOrFail

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -13,11 +13,10 @@ class ProfilesController extends Controller
     }    
 
     public function index($user)
-    {
-        
+    {      
         //dd($user); //Get what we are passing in
         //dd(User::find($user)); // Get the actual user
-        $user = User::find($user);
+        $user = User::findOrFail($user);
         //Passing data into the profile view, as a second argument, passing an array
         return view('profile',['user' => $user]);
     }
